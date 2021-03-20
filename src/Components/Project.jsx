0e6eb375bf9fd353c8b346eb8411228838ec0156
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-03-20 13:20:31 +0100

    Fix parallax, made links show dynamically upd. cnt

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -32,9 +32,11 @@ const Project = (props) => {
 
     useEffect(()=>{
         screenSizeHandler()
+        scrollHandler()
         setTimeout(() => {
             screenSizeHandler()
-        }, 100);
+            scrollHandler()
+        }, 120);
         window.addEventListener('resize', screenSizeHandler)
         window.addEventListener('orientationchange', screenSizeHandler)
         
@@ -72,18 +74,18 @@ const Project = (props) => {
         <div className={`${props.left ? "pr-8 sm:pr-12 md:pr-16 lg:pr-56 xl:pr-100 2xl:pr-136" : "pl-8 sm:pl-12 md:pl-16 lg:pl-56 xl:pl-100 2xl:pl-136"} mb-52 `}>
             {/* <img className={`${props.left ? "right-side-border" : "left-side-border"} object-cover h-80 sm:h-96 md:h-104 xl:h-128 w-full `} src={props.image} alt=""/> */}
             <div ref={imageContainer} className={`${props.left ? "right-side-border" : "left-side-border"} image${props.id} transition-fix overflow-hidden h-80 sm:h-96 md:h-104 xl:h-116 2xl:h-124 w-full `} >
-                <img ref={imageEl} onLoad={screenSizeHandler} className={`w-full relative image-child${props.id}`} style={{transform: `translate(0, ${plxOffset}px)`, top: `-${(imageHeight - containerHeight) + (containerHeight / 6)}px`}} src={imageAspectRatio === 'long' ? props.imageLong : imageAspectRatio === 'mid' ? props.imageMid : props.imageWide} alt=""/>
+                <img ref={imageEl} onLoad={screenSizeHandler, scrollHandler} className={`w-full relative image-child${props.id}`} style={{transform: `translate(0, ${plxOffset}px)`, top: `-${(imageHeight - containerHeight) + (containerHeight / 6)}px`}} src={imageAspectRatio === 'long' ? props.imageLong : imageAspectRatio === 'mid' ? props.imageMid : props.imageWide} alt=""/>
             </div>
                 <div className={`${props.left ? "right-side-border mr-8 sm:mr-12 md:ml-12 lg:ml-16 md:rounded-3xl" : "left-side-border ml-8 sm:ml-12 md:mr-12 lg:mr-16 md:ml-auto md:rounded-3xl "} " md:max-w-xl bg-white bg-opacity-70 bd-blur p-8 md:p-12 -mt-8 "`} >
                         <h3 className="text-3xl md:text-4xl mb-8 font-bold">{props.name}</h3>
                         <p className="pb-8">{props.body}</p>
-                        <div className=" mb-2 flex flex-row items-center">
+                        <div className={` mb-2 flex flex-row items-center ${props.link ? "" : "hidden"}`}>
                             <MdWeb size="1.7rem" />
-                            <a className="ml-2 underline" href={props.link}>Visit the site</a>
+                            <a target="_blank" rel="noreferrer noopener" className="ml-2 underline" href={props.link}>Visit the site</a>
                         </div>
-                        <div className="flex flex-row items-center">
+                        <div className={`flex flex-row items-center ${props.github ? "" : "hidden"}`}>
                             <FaGithub size="1.7rem" />
-                            <a className="underline ml-2" href={props.github}>Or check out the github repository</a>
+                            <a target="_blank" rel="noreferrer noopener"  className="underline ml-2" href={props.github}>Or check out the github repository</a>
                         </div>
                 </div>
         </div>
