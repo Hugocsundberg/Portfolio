e2a6ac4e7d8f2461efc1506ed0b1f4c80e80631f
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-03-20 11:07:20 +0100

    Downgrade autoprefixer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ import tree from './Components/tree.png'
 import treeWide from './Components/treeWide.png'
 import treeMid from './Components/treeMid.png'
 
+//FilmHelg
+import filmHelgLong from './Components/ProjectImages/filmhelg_long.jpg'
+import filmHelgWide from './Components/ProjectImages/filmhelg_wide.jpg'
+import filmHelgMid from './Components/ProjectImages/filmhelg_mid.jpg'
+// //this
+import thisLong from './Components/ProjectImages/this_long.jpeg'
+import thisWide from './Components/ProjectImages/this_wide.jpeg'
+import thisMid from './Components/ProjectImages/this_mid.jpeg'
+
 
 function App() {
 
@@ -16,8 +25,8 @@ function App() {
     <div className="App bg-gradient-to-b from-blue-100 to-purple-100 font-sans">
       <Hero className="animate-me" />
       <FirstProject/>
-      <Project id="2" name="SaladBowl" imageLong={tree} imageMid={treeMid} imageWide={treeWide} body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, gravida neque massa bibendum erat laoreet ornare vitae elementum. Diam porta massa in eget sed congue pretium at arcu. Scelerisque etiam tristique pulvinar dui eleifend praesent mi mi. Eu mauris quam vel condimentum egestas habitant malesuada vitae diam."/>
-      <Project id="3" left={true} name="SaladBowl" imageLong={tree} imageMid={treeMid} imageWide={treeWide} body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, gravida neque massa bibendum erat laoreet ornare vitae elementum. Diam porta massa in eget sed congue pretium at arcu. Scelerisque etiam tristique pulvinar dui eleifend praesent mi mi. Eu mauris quam vel condimentum egestas habitant malesuada vitae diam."/>
+      <Project id="2" name="SaladBowl" imageLong={filmHelgLong} imageMid={filmHelgMid} imageWide={filmHelgWide} body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, gravida neque massa bibendum erat laoreet ornare vitae elementum. Diam porta massa in eget sed congue pretium at arcu. Scelerisque etiam tristique pulvinar dui eleifend praesent mi mi. Eu mauris quam vel condimentum egestas habitant malesuada vitae diam."/>
+      <Project id="3" left={true} name="SaladBowl" imageLong={thisLong} imageMid={thisMid} imageWide={thisWide} body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, gravida neque massa bibendum erat laoreet ornare vitae elementum. Diam porta massa in eget sed congue pretium at arcu. Scelerisque etiam tristique pulvinar dui eleifend praesent mi mi. Eu mauris quam vel condimentum egestas habitant malesuada vitae diam."/>
       <AboutMe body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, gravida neque massa bibendum erat laoreet ornare vitae elementum. Diam porta massa in eget sed congue pretium at arcu. Scelerisque etiam tristique pulvinar dui eleifend praesent mi mi. Eu mauris quam vel condimentum egestas habitant malesuada vitae diam." />
     </div>
   );
--- src/Components/FirstProject.jsx
+++ src/Components/FirstProject.jsx
@@ -4,12 +4,15 @@ import leafs from './leaf.jpeg'
 import tree from './tree.png'
 import treeWide from './treeWide.png'
 import treeMid from './treeMid.png'
+import speedFeedLong from './ProjectImages/speed_feed_long.jpg'
+import speedFeedWide from './ProjectImages/speed_feed_wide.jpg'
+import speedFeedMid from './ProjectImages/speed_feed_mid.jpg'
 
 const FirstProject = () => {
     return (
         <div className="bg-gradient-to-b gradient-top">
             <h2 id="projectSection" className=" mb-8 ml-8 md:md-12 md:ml-12 text-5xl md:text-6xl font-bold">Projects.</h2>
-            <Project id="1" link="" github="" imageLong={tree} imageMid={treeMid} imageWide={treeWide} name="WORKHARD" left={true} body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, gravida neque massa bibendum erat laoreet ornare vitae elementum. Diam porta massa in eget sed congue pretium at arcu. Scelerisque etiam tristique pulvinar dui eleifend praesent mi mi. Eu mauris quam vel condimentum egestas habitant malesuada vitae diam." />
+            <Project id="1" link="" github="" imageLong={speedFeedLong} imageMid={speedFeedMid} imageWide={speedFeedWide} name="WORKHARD" left={true} body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, gravida neque massa bibendum erat laoreet ornare vitae elementum. Diam porta massa in eget sed congue pretium at arcu. Scelerisque etiam tristique pulvinar dui eleifend praesent mi mi. Eu mauris quam vel condimentum egestas habitant malesuada vitae diam." />
         </div>
     );
 }
