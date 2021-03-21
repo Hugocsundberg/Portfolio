226efe531110c542aac2ac0f4823b7524c722004
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-03-21 13:12:39 +0100

    Fix image load problem

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -44,14 +44,17 @@ const Project = (props) => {
         window.addEventListener('orientationchange', screenSizeHandler)
         
         let options = {
-            rootMargin: '0px',
+            rootMargin: '200px',
             threshold: [0, 0]
           }
       
         const intersectionHandler = (entry) => {
+            // console.log(entry)
             if(entry[0].isIntersecting === true) {
                 setShouldScroll(true)
                 setscrollListenerIsSet(true)
+                imageContainer.current.classList.add('animate-fadeIn')
+                
             } else if(entry[0].isIntersecting === false) {
                 setShouldScroll(false)
             }
@@ -77,7 +80,7 @@ const Project = (props) => {
         <div className={`${props.left ? "pr-8 sm:pr-12 md:pr-16 lg:pr-56 xl:pr-100 2xl:pr-136" : "pl-8 sm:pl-12 md:pl-16 lg:pl-56 xl:pl-100 2xl:pl-136"} mb-52 `}>
             {/* <img className={`${props.left ? "right-side-border" : "left-side-border"} object-cover h-80 sm:h-96 md:h-104 xl:h-128 w-full `} src={props.image} alt=""/> */}
             <div ref={imageContainer} className={`${props.left ? "right-side-border" : "left-side-border"} image${props.id} transition-fix overflow-hidden h-80 sm:h-96 md:h-104 xl:h-116 2xl:h-124 w-full `} >
-                <img ref={imageEl} onLoad={screenSizeHandler, scrollHandler} className={`w-full relative image-child${props.id}`} style={{transform: `translate(0, ${plxOffset}px)`, top: `-${(imageHeight - containerHeight) + (containerHeight / 6)}px`}} src={imageAspectRatio === 'long' ? props.imageLong : imageAspectRatio === 'mid' ? props.imageMid : props.imageWide} alt=""/>
+                <img ref={imageEl} onLoad={screenSizeHandler, scrollHandler} className={` w-full relative image-child${props.id}`} style={{transform: `translate(0, ${plxOffset}px)`, top: `-${(imageHeight - containerHeight) + (containerHeight / 6)}px`}} src={imageAspectRatio === 'long' ? props.imageLong : imageAspectRatio === 'mid' ? props.imageMid : props.imageWide} alt=""/>
             </div>
                 <div className={`${props.left ? "right-side-border mr-8 sm:mr-12 md:ml-12 lg:ml-16 md:rounded-3xl" : "left-side-border ml-8 sm:ml-12 md:mr-12 lg:mr-16 md:ml-auto md:rounded-3xl "} " md:max-w-xl bg-white bg-opacity-70 bd-blur p-8 md:p-12 -mt-8 "`} >
                         <h3 className="text-3xl md:text-4xl mb-8 font-bold">{props.name}</h3>
