d3c7d4935f3deb4e73f45af9e427558e974ef429
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-03-21 22:19:32 +0100

    Add routes for languages

--- src/Components/FirstProject.jsx
+++ src/Components/FirstProject.jsx
@@ -6,10 +6,7 @@ import speedFeedMid from './ProjectImages/speed_feed_mid.jpg'
 
 const FirstProject = () => {
     return (
-        <div className="bg-gradient-to-b gradient-top">
-            <h2 id="projectSection" className=" mb-8 ml-8 md:md-12 md:ml-12 text-5xl md:text-6xl font-bold">Projects.</h2>
-            <Project link="https://speed-feed.herokuapp.com/" github="https://github.com/Hugocsundberg/Speed-Feed_Social_Network" id="1" imageLong={speedFeedLong} imageMid={speedFeedMid} imageWide={speedFeedWide} name="Speed Feed" left={true} body="Speed Feed is a social network designed and created by myself with native PHP, JS and CSS.The project is hosted on Heroku with a Postgres database." />
-        </div>
+        
     );
 }
 
