878ca9b3d166dfcd206ab2b8b2fb3e78f4edbfd1
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-03-21 22:19:51 +0100

    Add routing for languages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import AboutMe from './Components/AboutMe';
-import FirstProject from './Components/FirstProject';
 import Hero from './Components/Hero';
-import MoveShape from './Components/MoveShape';
 import Project from './Components/Project';
-import leafs from './Components/leaf.jpeg'
-import tree from './Components/tree.png'
-import treeWide from './Components/treeWide.png'
-import treeMid from './Components/treeMid.png'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+//Speed Feed
+import speedFeedLong from './Components/ProjectImages/speed_feed_long.jpg'
+import speedFeedWide from './Components/ProjectImages/speed_feed_wide.jpg'
+import speedFeedMid from './Components/ProjectImages/speed_feed_mid.jpg'
 
 //FilmHelg
 import filmHelgLong from './Components/ProjectImages/filmhelg_long.jpg'
@@ -24,10 +29,28 @@ function App() {
   return (
     <div className="App bg-gradient-to-b from-blue-100 to-purple-100 font-sans">
       <Hero className="animate-me" />
-      <FirstProject/>
-      <Project link="https://filmhelg.netlify.app/" github="https://github.com/Hugocsundberg/FilmHelg_Tailwind" id="2" name="Filmhelg" imageLong={filmHelgLong} imageMid={filmHelgMid} imageWide={filmHelgWide} body="School project together with class 'informationsproduktion' (IP). Assignment was to create an imaginary event-page for an existing company. (Filmstaden in our case) The project uses Tailwind for styling and browserify, postCSS and a costum html-minifier for bundling and minification. The site is live on netlify: https://filmhelg.netlify.app"/>
-      <Project link="" github="https://github.com/Hugocsundberg/portfolio_react_tailwind" id="3" left={true} name=".this" imageLong={thisLong} imageMid={thisMid} imageWide={thisWide} body="This portfolio is built with react and styled using Tailwindcss. Text animation on the top section is made with anime.js. The parallax effect is built using react hooks, 'scroll' eventlisteners and intersection observer API."/>
-      <AboutMe github="https://github.com/Hugocsundberg" body="Hi, I'm 25 years old and is studying fullstack web development at Yrgo. I'm a technology nerd and have previous experience of production management in web development. After a couple years of being in the IT and webb sphere i can't see myself working in another field. Beyond programming I enjoy spending time at the gym or playing disc golf. I have a big interest for design and UX and believe that a good design and UX is mandatory for good functionality to deliver value." />
+      <Router>
+        <Switch>
+          <Route path="/en">
+            <div className="bg-gradient-to-b gradient-top">
+              <h2 id="projectSection" className=" mb-8 ml-8 md:md-12 md:ml-12 text-5xl md:text-6xl font-bold">Projects.</h2>
+              <Project link="https://speed-feed.herokuapp.com/" github="https://github.com/Hugocsundberg/Speed-Feed_Social_Network" id="1" imageLong={speedFeedLong} imageMid={speedFeedMid} imageWide={speedFeedWide} name="Speed Feed" left={true} body="Speed Feed is a social network designed and created by myself with native PHP, JS and CSS.The project is hosted on Heroku with a Postgres database." />
+            </div>
+            <Project link="https://filmhelg.netlify.app/" github="https://github.com/Hugocsundberg/FilmHelg_Tailwind" id="2" name="Filmhelg" imageLong={filmHelgLong} imageMid={filmHelgMid} imageWide={filmHelgWide} body="School project together with class 'informationsproduktion' (IP). Assignment was to create an imaginary event-page for an existing company. (Filmstaden in our case) The project uses Tailwind for styling and browserify, postCSS and a costum html-minifier for bundling and minification. The site is live on netlify: https://filmhelg.netlify.app"/>
+            <Project link="" github="https://github.com/Hugocsundberg/portfolio_react_tailwind" id="3" left={true} name=".this" imageLong={thisLong} imageMid={thisMid} imageWide={thisWide} body="This portfolio is built with react and styled using Tailwindcss. Text animation on the top section is made with anime.js. The parallax effect is built using react hooks, 'scroll' eventlisteners and intersection observer API."/>
+            <AboutMe github="https://github.com/Hugocsundberg" body="Hi, I'm 25 years old and is studying fullstack web development at Yrgo. I'm a technology nerd and have previous experience of production management in web development. After a couple years of being in the IT and webb sphere i can't see myself working in another field. Beyond programming I enjoy spending time at the gym or playing disc golf. I have a big interest for design and UX and believe that a good design and UX is mandatory for good functionality to deliver value." />
+          </Route>
+          <Route path="/sv">
+            <div className="bg-gradient-to-b gradient-top">
+              <h2 id="projectSection" className=" mb-8 ml-8 md:md-12 md:ml-12 text-5xl md:text-6xl font-bold">Projekt.</h2>
+              <Project link="https://speed-feed.herokuapp.com/" github="https://github.com/Hugocsundberg/Speed-Feed_Social_Network" id="1" imageLong={speedFeedLong} imageMid={speedFeedMid} imageWide={speedFeedWide} name="Speed Feed" left={true} body="Speed Feed is a social network designed and created by myself with native PHP, JS and CSS.The project is hosted on Heroku with a Postgres database." />
+            </div>
+            <Project link="https://filmhelg.netlify.app/" github="https://github.com/Hugocsundberg/FilmHelg_Tailwind" id="2" name="Filmhelg" imageLong={filmHelgLong} imageMid={filmHelgMid} imageWide={filmHelgWide} body="School project together with class 'informationsproduktion' (IP). Assignment was to create an imaginary event-page for an existing company. (Filmstaden in our case) The project uses Tailwind for styling and browserify, postCSS and a costum html-minifier for bundling and minification. The site is live on netlify: https://filmhelg.netlify.app"/>
+            <Project link="" github="https://github.com/Hugocsundberg/portfolio_react_tailwind" id="3" left={true} name=".this" imageLong={thisLong} imageMid={thisMid} imageWide={thisWide} body="This portfolio is built with react and styled using Tailwindcss. Text animation on the top section is made with anime.js. The parallax effect is built using react hooks, 'scroll' eventlisteners and intersection observer API."/>
+            <AboutMe github="https://github.com/Hugocsundberg" body="Hi, I'm 25 years old and is studying fullstack web development at Yrgo. I'm a technology nerd and have previous experience of production management in web development. After a couple years of being in the IT and webb sphere i can't see myself working in another field. Beyond programming I enjoy spending time at the gym or playing disc golf. I have a big interest for design and UX and believe that a good design and UX is mandatory for good functionality to deliver value." />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
