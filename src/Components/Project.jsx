3500cee5232f861fd65955430251ae79903c019f
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-03-25 12:09:26 +0100

    Remove debugging logs

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -37,7 +37,6 @@ const Project = (props) => {
         const interval = setInterval(() => {
             if(i < 30) {
                 i++
-                console.log('now!')
                 screenSizeHandler()  
             }
             else {
@@ -45,15 +44,12 @@ const Project = (props) => {
             }
         }, 70);
         setTimeout(() => {
-            console.log('now!')
             screenSizeHandler()
         }, 3600);
         setTimeout(() => {
-            console.log('now!')
             screenSizeHandler()
         }, 5000);
         setTimeout(() => {
-            console.log('now!')
             screenSizeHandler()
         }, 7000);
         window.addEventListener('resize', screenSizeHandler)
